799c74901503acbeed5ce3d91bfc104614e29a86
Ricardo Lafuente <r@manufacturaindependente.org>
2015-09-30 17:11:56 +0200

    Add interactivity and popups to map

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -2,27 +2,18 @@
 /*global L */
 
 function getColor(d) {
-    if (d == '-') { return '#ccc' }
-    if (d == '<5') { return '#FD8D3C' }
+    if (d === '-') { return '#222'; }
+    if (d === '<5') { return '#FD8D3C'; }
     return d > 30 ? '#800026' :
            d > 20  ? '#BD0026' :
            d > 10  ? '#E31A1C' :
            d > 5  ? '#FC4E2A' :
            d > 20   ? '#FEB24C' :
            d > 10   ? '#FED976' :
-                      '#FFEDA0';
+          '#ccc';
 }
 
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.score),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
+
 
 (function (window, document, L, undefined) {
 	'use strict';
@@ -36,15 +27,81 @@ function style(feature) {
 	});
 
 	/* add default stamen tile layer */
+        /*
 	new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
 		minZoom: 0,
 		maxZoom: 18,
 		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
 	}).addTo(map);
+        */
+	new L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png', {
+                subdomains: '0123',
+		minZoom: 0,
+		maxZoom: 18
+	}).addTo(map);
+
+
+
+        var info = L.control();
+        var geojsonLayer;
+
+        function style(feature) {
+            return {
+                fillColor: getColor(feature.properties.score),
+                weight: feature.properties.score ? 1 : 0,
+                opacity: 0.3,
+                color: 'white',
+                dashArray: '',
+                fillOpacity: feature.properties.score ? 0.7 : 0
+            };
+        }
+
+        // Interaction logic from https://jsfiddle.net/eaj6h/11/
+        function highlightFeature(e) {
+            var layer = e.target;
+
+            layer.setStyle({
+                weight: 5,
+                color: 'white',
+                dashArray: '',
+                fillOpacity: 0.7
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera) {
+                layer.bringToFront();
+            }
+
+            // info.update(layer.feature.properties);
+        }
+
+
+        function resetHighlight(e) {
+            geojsonLayer.resetStyle(e.target);
+            // info.update();
+        }
+
+        function zoomToFeature(e) {
+            map.fitBounds(e.target.getBounds());
+        }
+
+        function onEachFeature(feature, layer) {
+            var popupContent = '<h3>' + feature.properties.name + '</h3> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p><a href="/countries/' + feature.id + '">Come visit!</p>';
+            layer.bindPopup(popupContent, {autopan: true});
+
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+            });
+        }
 
         // https://gis.stackexchange.com/a/102125
-        var geojsonLayer = new L.GeoJSON.AJAX('data/mockdata.geo.json', {style: style});       
+        geojsonLayer = new L.GeoJSON.AJAX('data/mockdata.geo.json', {
+                              style: style,
+                              onEachFeature: onEachFeature
+        });       
         geojsonLayer.addTo(map);
+
+
 
 
 }(window, document, L));
