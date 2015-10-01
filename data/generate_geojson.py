52e525cb651382e45c2e9b08763e4dfe55a4db17
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-01 15:13:09 +0200

    Fix data filter script to work with country codes

--- data/generate_geojson.py
+++ data/generate_geojson.py
@@ -6,21 +6,18 @@ import csv
 geodata = json.loads(open("ghi-countries.geo.json", "r").read())
 scores = csv.DictReader(open("ghi-scores.csv", "r"))
 
-country_names = [entry['properties']['name'] for entry in geodata['features']]
-
-for entry in geodata['features']:
-    print entry
-    print
+country_codes = [entry['id'] for entry in geodata['features']]
 
 for s in scores:
-    if s['country'] in country_names:
+    if s['countrycode3'] in country_codes:
         for entry in geodata['features']:
-            if entry['properties']['name'] == s['country']:
+            if entry['id'] == s['countrycode3']:
+                entry['properties']['name'] = s['country']
                 entry['properties']['score'] = s['score2015']
                 break
     else:
         print "Not found: " + s['country']
 
-f = open("mockdata.geo.json", 'w')
+f = open("../site/app/data/mockdata.geo.json", 'w')
 f.write(json.dumps(geodata, indent=2))
 f.close()
