f2800b2592ce7bc91ac1d52962454160d03a844d
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-04 18:50:50 +0200

    Support for static pages

--- tools/render_templates.py
+++ tools/render_templates.py
@@ -12,21 +12,22 @@ TODO:
 
 '''
 
+import os
 import jinja2
 import json
 import csv
 import codecs
+import markdown
 
-config_file = "settings.conf"
-output_dir = "_output"
-repo_dir = "repos"
-datasets_dir = "datasets"
-files_dir = "download"
-themes_dir = "themes"
+# Specify the static page names to render
+static_pages = ["about", "contact", "hunger", "methodology"]
 
 # set up Jinja
 template_dir = "../site/jinja_templates"
 env = jinja2.Environment(loader=jinja2.FileSystemLoader([template_dir]))
+
+csvdata = csv.reader(open("../data/messages.csv", 'r'))
+messages = {label: text for label, text, text_de in csvdata}
 
 
 def get_level_from_score(score):
@@ -49,30 +50,13 @@ def get_level_from_score(score):
         print "Unexpected score: ", score
     return level
 
-'''
-
-  return d >= 50 ? '#ab0635' :
-    d >= 35  ? '#e9841d' :
-    d >= 20  ? '#fbe0c7' :
-    d >= 10  ? '#bedcb3' :
-    d >= 0   ? '#4caf45' :
-    '#eaeaea';
-
-'''
-
 
 def create_index_page():
-    '''Generates the index page with the list of available packages.
-    Accepts a list of pkg_info dicts, which are generated with the
-    process_datapackage function.'''
     template = env.get_template("index.html")
 
     table_entries = json.loads(open("../data/table_data.json", "r").read())["data"]
     for entry in table_entries:
         entry['level'] = get_level_from_score(entry['score'])
-
-    csvdata = csv.reader(open("../data/messages.csv", 'r'))
-    messages = {label: text for label, text, text_de in csvdata}
 
     context = {"table_entries": table_entries,
                "m": messages,
@@ -82,5 +66,23 @@ def create_index_page():
     f.write(contents)
     f.close()
 
+
+def create_static_page(name):
+    template = env.get_template("static.html")
+
+    content = markdown.markdown(open("../data/pages/%s.md" % name, "r").read())
+    context = {"md_content": content,
+               "m": messages,
+               }
+    contents = template.render(**context)
+    dirname = "../site/app/html/%s/" % name
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    f = codecs.open(os.path.join(dirname, "index.html"), 'w', 'utf-8')
+    f.write(contents)
+    f.close()
+
 if __name__ == "__main__":
     create_index_page()
+    for p in static_pages:
+        create_static_page(p)
