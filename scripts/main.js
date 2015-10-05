98aaed26b457672e2439ec1d585796be25de5f5d
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-05 18:20:41 +0200

    Update 2015-10-05T16:20:36.851Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,146 @@
-function getColor(e){return"-"===e?"#808080":"<5"===e?"#4caf45":e>=50?"#ab0635":e>=35?"#e9841d":e>=20?"#fbe0c7":e>=10?"#bedcb3":e>=0?"#4caf45":"#eaeaea"}function getSeverity(e){return"-"===e?"No data":"<5"===e?"Low":e>=50?"Extremely alarming":e>=35?"Alarming":e>=20?"Serious":e>=10?"Moderate":e>=0?"Low":"Not calculated"}$(document).foundation(),function(e,o,t,r){"use strict";function a(e){return{fillColor:getColor(e.properties.score),weight:e.properties.score?1:0,opacity:.3,color:"white",dashArray:"",fillOpacity:e.properties.score?.7:0}}function n(e){var o=e.target;o.setStyle({weight:5,color:"white",dashArray:"",fillOpacity:.7}),t.Browser.ie||t.Browser.opera||o.bringToFront()}function i(e){l.resetStyle(e.target)}function c(e,o){var t;t="nc"!==e.properties.score?"<h4>"+e.properties.name+"</h4> <p>Score: <strong>"+e.properties.score+"</strong></p> <p>Level: <strong>"+getSeverity(e.properties.score)+'</strong></p> <p><a class="button small radius" href="/countries/'+e.id+'">Find out more</a></p>':"<h4>"+e.properties.name+"</h4> <p>Score: <strong> Not calculated</p>",o.bindPopup(t,{autopan:!0}),o.on({mouseover:n,mouseout:i}),o._leaflet_id=e.id}t.Icon.Default.imagePath="images/";var s=t.map("map",{center:[52.5377,13.3958],scrollWheelZoom:!1,boxZoom:!1,zoom:4});new t.tileLayer("http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png",{subdomains:"0123",minZoom:0,maxZoom:18}).addTo(s);var l;t.control();l=new t.GeoJSON.AJAX("data/countrydata-2015.geo.json",{style:a,onEachFeature:c}),l.addTo(s),$(o).ready(function(){$("#country-table tbody").on("click","tr",function(e){var o=l.getLayer(this.id);s.setView(o.getBounds().getCenter()),o.openPopup()}),$("#year-drop li a").click(function(e){var o=this.className;l.clearLayers(),l=new t.GeoJSON.AJAX("data/countrydata-"+o+".geo.json",{style:a,onEachFeature:c}),l.addTo(s),$("#year-button").text(o)})})}(window,document,L);
+// Foundation JavaScript
+$(document).foundation();
+
+/*jslint browser: true*/
+/*global L */
+
+function getColor(d) {
+  if (d === '-') { return '#808080'; }
+  if (d === '<5') { return '#4caf45'; }
+  return d >= 50 ? '#ab0635' :
+    d >= 35  ? '#e9841d' :
+    d >= 20  ? '#fbe0c7' :
+    d >= 10  ? '#bedcb3' :
+    d >= 0   ? '#4caf45' :
+    '#eaeaea';
+}
+
+function getSeverity(d) {
+  if (d === '-') { return 'No data'; }
+  if (d === '<5') { return 'Low'; }
+  return d >= 50 ? 'Extremely alarming' :
+    d >= 35  ? 'Alarming' :
+    d >= 20  ? 'Serious' :
+    d >= 10  ? 'Moderate' :
+    d >= 0   ? 'Low' :
+    'Not calculated';
+}
+
+
+
+(function (window, document, L, undefined) {
+  'use strict';
+
+  L.Icon.Default.imagePath = 'images/';
+
+  /* create leaflet map */
+  var map = L.map('map', {
+    center: [52.5377, 13.3958],
+    scrollWheelZoom: false,
+    boxZoom: false,
+    zoom: 4
+  });
+
+  new L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png', {
+      subdomains: '0123',
+      minZoom: 0,
+      maxZoom: 18
+      }).addTo(map);
+
+  var info = L.control();
+  var geojsonLayer;
+
+  function style(feature) {
+    return {
+      fillColor: getColor(feature.properties.score),
+      weight: feature.properties.score ? 1 : 0,
+      opacity: 0.3,
+      color: 'white',
+      dashArray: '',
+      fillOpacity: feature.properties.score ? 0.7 : 0
+    };
+  }
+
+  // Interaction logic from https://jsfiddle.net/eaj6h/11/
+  function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      color: 'white',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
+    }
+
+    // info.update(layer.feature.properties);
+  }
+
+
+  function resetHighlight(e) {
+    geojsonLayer.resetStyle(e.target);
+    // info.update();
+  }
+
+  function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+  }
+
+  function onEachFeature(feature, layer) {
+
+    // set up popups
+    var popupContent;
+    if (feature.properties.score !== 'nc') {
+      popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p>Level: <strong>' + getSeverity(feature.properties.score) + '</strong></p> <p><a class="button small radius" href="/countries/' + feature.id + '">Find out more</a></p>';
+    } else {
+      popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong> Not calculated</p>';
+    }
+
+
+
+    layer.bindPopup(popupContent, {autopan: true});
+    // set up mouseover highlights
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+    });
+    // make features accessible by id
+    // https://stackoverflow.com/a/28618177
+    layer._leaflet_id = feature.id;
+  }
+
+  // https://gis.stackexchange.com/a/102125
+  geojsonLayer = new L.GeoJSON.AJAX('data/countrydata-2015.geo.json', {
+    style: style,
+    onEachFeature: onEachFeature
+  });       
+  geojsonLayer.addTo(map);
+
+  $(document).ready(function() {
+    $('#country-table tbody').on( 'click', 'tr', function (ev) {
+      // clicking on a country in the table focuses the map on it
+      var f = geojsonLayer.getLayer(this.id);
+      map.setView(f.getBounds().getCenter());
+      f.openPopup();
+    } );
+    $('#year-drop li a').click( function(ev) {
+      // year dropdown refreshes map
+      var year = this.className;
+      geojsonLayer.clearLayers();geojsonLayer = new L.GeoJSON.AJAX('data/countrydata-' + year + '.geo.json', {
+        style: style,
+        onEachFeature: onEachFeature
+      });       
+      geojsonLayer.addTo(map);
+      $('#year-button').text(year);
+
+    });
+  });
+
+
+
+
+}(window, document, L));
+
