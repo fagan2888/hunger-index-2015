0e358b87c2de5107406d4496e7be4b7618bdf3ca
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-08 15:18:18 +0200

    Embed page links open in new windows

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -104,7 +104,14 @@ function getSeverityClass(d) {
     // set up popups
     var popupContent;
     if (feature.properties.score !== 'nc') {
+
+      // are we in the embed page? If so, links open in a new window
+      var url = window.location.href;
+      if (url.indexOf('embed') > -1) {
+      popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p>Level: <strong>' + getSeverity(feature.properties.score) + '</strong></p> <p><a class="button small radius" target="_blank" href="countries/' + feature.id + '">Find out more</a></p>';
+      } else {
       popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p>Level: <strong>' + getSeverity(feature.properties.score) + '</strong></p> <p><a class="button small radius" href="countries/' + feature.id + '">Find out more</a></p>';
+      }
     } else {
       popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong> Not calculated</p>';
     }
@@ -179,6 +186,9 @@ function getSeverityClass(d) {
       populateTable(year);
 
     });
+
+
+
   });
 
 
