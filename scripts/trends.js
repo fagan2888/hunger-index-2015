075a35e321e14ddc003c4976f14d3f4f00028326
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-08 13:15:58 +0200

    Update 2015-10-08T11:15:53.525Z

--- scripts/trends.js
+++ scripts/trends.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
   var table = $('#trends-table').DataTable( {
-    'paging': false,
     'ajax': '../data/trends-2015.json',
     'columns': [
-    { data: 'name' },
-    { data: 'undernourished' },
-    { data: 'stunting' },
-    { data: 'wasting' },
-    { data: 'mortality' },
-    { data: 'score' }
+      { data: 'name' },
+      { data: 'undernourished' },
+      { data: 'stunting' },
+      { data: 'wasting' },
+      { data: 'mortality' },
+      { data: 'zone' },
+      { data: 'score' }
     ],
-    "dom": '<"trends-toolbar">frtip'
+    // hide zone column
+    'columnDefs': [ {'targets': [5], 'visible': false} ],
+    "dom": '<"trends-toolbar">frtip',
+    'paging': false,
+    'info': false
   } );
 
-  $("div.trends-toolbar").html('<label for="year">Year</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select>');
+  $("div.trends-toolbar").html('<label for="year">Year</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select> <label for="zone">Zone</label> <select name="zone" id="zone"> <option value="all" selected>All countries</option> <option value="MENA">Near East and North Africa</option> <option value="WAF">West Africa</option> <option value="CSAF">Central and Southern Africa</option> <option value="EAF">East Africa</option> <option value="SA">South America</option> <option value="CAR">Central America and the Caribbean</option> <option value="SEA">South, East and Southeast Asia</option> <option value="EEFSU">Eastern Europe and the Commonwealth of Independent States</option> </select>');
 
   $("div.trends-toolbar #year option").click( function () { 
     var year = this.value; 
     table.ajax.url('../data/trends-' + year + '.json').load();
+  });
+
+  $("div.trends-toolbar #zone option").click( function () { 
+    var zone = this.value; 
+    table.column(5).search(zone).draw()
+
   });
 
 
@@ -25,7 +35,6 @@ $(document).ready(function() {
 
 
 /*
-<label for="year">Year</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select>
 
 <label for="year">Year</label>
 <select name="year" id="year">
@@ -35,5 +44,25 @@ $(document).ready(function() {
   <option value="1995">1995</option>
   <option value="1990">1990</option>
 </select>
+
+<label for="zone">Zone</label>
+<select name="zone" id="zone">
+  <option value="all" selected>All countries</option> 
+  <option value="MENA">Near East and North Africa</option>
+  <option value="WAF">West Africa</option>
+  <option value="CSAF">Central and Southern Africa</option>
+  <option value="EAF">East Africa</option>
+  <option value="SA">South America</option>
+  <option value="CAR">Central America and the Caribbean</option>
+  <option value="SEA">South, East and Southeast Asia</option>
+  <option value="EEFSU">Eastern Europe and the Commonwealth of Independent States</option>
+</select>
+
+
+
+
+
+
+
 
 */
