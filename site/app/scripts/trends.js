3fca7f5e311a09fb4ca1cfe92fcf05812bceb027
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-11 14:37:30 +0200

    Fix German text in Trends page

--- site/app/scripts/trends.js
+++ site/app/scripts/trends.js
@@ -76,7 +76,17 @@ $(document).ready(function() {
     'info': false
   } );
 
-  $('div.trends-toolbar').html('<div class="large-2 columns toolbar-column"><label for="year">Year</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select></div><div class="large-5 columns toolbar-column"><label for="zone">Zone</label> <select name="zone" id="zone"> <option value="all" selected>All countries</option> <option value="MENA">Near East and North Africa</option> <option value="WAF">West Africa</option> <option value="CSAF">Central and Southern Africa</option> <option value="EAF">East Africa</option> <option value="SA">South America</option> <option value="CAR">Central America and the Caribbean</option> <option value="SEA">South, East and Southeast Asia</option> <option value="EEFSU">Eastern Europe and the Commonwealth of Independent States</option> </select></div>');
+  var toolbarHtml;
+  if (lang == "de") {
+    // Fix "Search" label
+    var label_html = $('#trends-table_filter label').html().replace("Search:", "Suchen:")
+    $('#trends-table_filter label').html(label_html);
+    toolbarHtml = '<div class="large-2 columns toolbar-column"><label for="year">Jahr</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select></div><div class="large-5 columns toolbar-column"><label for="zone">Zone</label> <select name="zone" id="zone"> <option value="all" selected>Alle Länder</option> <option value="MENA">Naher Osten und Nordafrika</option> <option value="WAF">West-Afrika</option> <option value="CSAF">Zentral- und Südafrika</option> <option value="EAF">Ostafrika</option> <option value="SA">Südamerika</option> <option value="CAR">Zentralamerika und Karibik</option> <option value="SEA">Südostasien</option> <option value="EEFSU">Osteuropa und Gemeinschaft Unabhängiger Staaten</option> </select></div>';
+  } else {
+    toolbarHtml = '<div class="large-2 columns toolbar-column"><label for="year">Year</label> <select name="year" id="year"> <option value="2015" selected>2015</option> <option value="2005">2005</option> <option value="2000">2000</option> <option value="1995">1995</option> <option value="1990">1990</option> </select></div><div class="large-5 columns toolbar-column"><label for="zone">Zone</label> <select name="zone" id="zone"> <option value="all" selected>All countries</option> <option value="MENA">Near East and North Africa</option> <option value="WAF">West Africa</option> <option value="CSAF">Central and Southern Africa</option> <option value="EAF">East Africa</option> <option value="SA">South America</option> <option value="CAR">Central America and the Caribbean</option> <option value="SEA">South, East and Southeast Asia</option> <option value="EEFSU">Eastern Europe and the Commonwealth of Independent States</option> </select></div>';
+  }
+  $('div.trends-toolbar').html(toolbarHtml);
+
 
   $('select#year').change(function () { 
     var year = this.value; 
@@ -107,6 +117,22 @@ $(document).ready(function() {
   <option value="1990">1990</option>
 </select>
 
+
+<label for="zone">Zone</label>
+<select name="zone" id="zone">
+  <option value="all" selected>Alle Länder</option> 
+  <option value="MENA">Naher Osten und Nordafrika</option>
+  <option value="WAF">West-Afrika</option>
+  <option value="CSAF">Zentral- und Südafrika</option>
+  <option value="EAF">Ostafrika</option>
+  <option value="SA">Südamerika</option>
+  <option value="CAR">Zentralamerika und Karibik</option>
+  <option value="SEA">Südostasien</option>
+  <option value="EEFSU">Osteuropa und Gemeinschaft Unabhängiger Staaten</option>
+</select>
+
+
+
 <label for="zone">Zone</label>
 <select name="zone" id="zone">
   <option value="all" selected>All countries</option> 
@@ -119,9 +145,6 @@ $(document).ready(function() {
   <option value="SEA">South, East and Southeast Asia</option>
   <option value="EEFSU">Eastern Europe and the Commonwealth of Independent States</option>
 </select>
-
-
-
 
 
 
